735ae60764b0d7ecfb75e961ff0840ae6c8622d8
James Lagattuta <jimmy.lagattuta@gmail.com>
2026-04-28 20:30:57 -0700

    portal updates

--- client/src/portal/pages/Dashboard.jsx
+++ client/src/portal/pages/Dashboard.jsx
@@ -1199,39 +1199,6 @@ function EmbeddedUserDetail({ userId, onBack }) {
   );
 }
 
-function StatCard({ title, value, subtext, glow }) {
-  return (
-    <div style={statCardStyle}>
-      <Glow color={glow} />
-
-      <div>
-        <div style={labelStyle}>{title}</div>
-        <div
-          style={{
-            marginTop: 20,
-            fontSize: "2.3rem",
-            fontWeight: 900,
-            letterSpacing: "-0.04em",
-            color: "#ffffff",
-          }}
-        >
-          {value}
-        </div>
-      </div>
-
-      <div
-        style={{
-          marginTop: 10,
-          color: "#cbd5e1",
-          fontSize: "0.95rem",
-        }}
-      >
-        {subtext}
-      </div>
-    </div>
-  );
-}
-
 function SectionMiniHeader({ title, subtitle }) {
   return (
     <div style={{ marginBottom: 18 }}>
